b7ab99f1d72084907af07f9b946ff67c8f3159b7
Himanshu Raina <72431465+himanshurainaa@users.noreply.github.com>
2026-07-19 02:36:16 +0530

    Time: 0 ms (100%), Space: 12.9 MB (78.85%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,14 +1,27 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
         int maxCount = 0;
-        for (const string& s : sentences) {
-            int spaces = 0;
-            for (char c : s) {
-                if (c == ' ') spaces++;
-            }
-            maxCount = max(maxCount, spaces + 1);
+        
+        // Use .size() to get the number of sentences
+        for (int i = 0; i < sentences.size(); i++) {
+            int wordCount = 0;
+            
+            // Count spaces to find word count (words = spaces + 1)
+            // This is a built-in approach using std::count
+            wordCount = count(sentences[i].begin(), sentences[i].end(), ' ') + 1;
+            
+            // Update maxCount
+            maxCount = max(maxCount, wordCount);
         }
+        
         return maxCount;
     }
 };

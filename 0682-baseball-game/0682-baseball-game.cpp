9a058eaaeebf21c6abe4f9e176e30fc81457ce06
Himanshu Raina <72431465+himanshurainaa@users.noreply.github.com>
2026-07-25 01:15:13 +0530

    Time: 0 ms (100%), Space: 11.8 MB (99.54%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int> record;
+        for (const string& op : operations) {
+            if (op == "+") {
+                int n = record.size();
+                record.push_back(record[n - 1] + record[n - 2]);
+            } else if (op == "D") {
+                record.push_back(2 * record.back());
+            } else if (op == "C") {
+                record.pop_back();
+            } else {
+                record.push_back(stoi(op));
+            }
+        }
+        int sum = 0;
+        for (int score : record) {
+            sum += score;
+        }
+        return sum;
+    }
+};

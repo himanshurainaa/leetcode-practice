7d6c3814361f7e29255f3d6bd0ee992ea416d8c6
Himanshu Raina <72431465+himanshurainaa@users.noreply.github.com>
2026-07-25 01:12:21 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.75%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth = 0;
+        for (const auto& customer : accounts) {
+            int currentWealth = 0;
+            for (int amount : customer) {
+                currentWealth += amount;
+            }
+            if (currentWealth > maxWealth) {
+                maxWealth = currentWealth;
+            }
+        }
+        return maxWealth;
+    }
+};

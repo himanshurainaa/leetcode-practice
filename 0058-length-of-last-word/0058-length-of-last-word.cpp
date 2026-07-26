e429056df4a19f6a16f3099ba6dbf5769b41e8a7
Himanshu Raina <72431465+himanshurainaa@users.noreply.github.com>
2026-07-26 23:42:40 +0530

    Time: 0 ms (100%), Space: 9 MB (35.32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -5,12 +5,10 @@ public:
         int n = s.length();
         int i = n - 1;
 
-        // Step 1: Skip trailing spaces
         while (i >= 0 && s[i] == ' ') {
             i--;
         }
 
-        // Step 2: Count the last word characters
         while (i >= 0 && s[i] != ' ') {
             count++;
             i--;

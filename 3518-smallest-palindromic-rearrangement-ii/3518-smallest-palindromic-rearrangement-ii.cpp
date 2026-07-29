55ad1d1a84dfef95ed59791af2e7bb794826ff7a
Himanshu Raina <72431465+himanshurainaa@users.noreply.github.com>
2026-07-30 02:24:57 +0530

    Time: 4 ms (97.71%), Space: 14.5 MB (93.13%) - LeetHub

--- 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
+++ 3518-smallest-palindromic-rearrangement-ii/3518-smallest-palindromic-rearrangement-ii.cpp
@@ -1,86 +1,85 @@
 class Solution {
 public:
     string smallestPalindrome(string s, int k) {
-        vector<int> count(26, 0);
-        for (char c : s) count[c - 'a']++;
-        
-        int oddCount = 0;
-        char midChar = 0;
-        for (int i = 0; i < 26; i++) {
-            if (count[i] % 2 != 0) {
-                oddCount++;
-                midChar = i + 'a';
-            }
-            count[i] /= 2;
-        }
-        
-        if (oddCount > 1) return "";
-        
-        int n = 0;
-        for (int x : count) n += x;
-        
-        string leftHalf = "";
-        long long targetK = k; 
-        
-        
-        for (int i = 0; i < n; i++) {
-            bool foundChar = false;
-            for (int j = 0; j < 26; j++) {
-                if (count[j] > 0) {
-                    count[j]--; 
-                    
-                    long long ways = calcWays(count, targetK);
-                    
-                    if (targetK <= ways) {
-                        leftHalf += (char)(j + 'a');
-                        foundChar = true;
-                        break; 
-                    } else {
-                        targetK -= ways;
-                        count[j]++; 
-                    }
-                }
-            }
-            if (!foundChar) return ""; 
-        }
-        
-        string rightHalf = leftHalf;
-        reverse(rightHalf.begin(), rightHalf.end());
-        
-        if (oddCount == 1) {
-            return leftHalf + midChar + rightHalf;
-        }
-        return leftHalf + rightHalf;
-    }
+        int n = s.size() / 2;
+        int ct[26] = {0};
 
-private:
-    long long calcWays(const vector<int>& count, long long limit) {
-        long long ways = 1;
-        int sum = 0;
-        
-        for (int x : count) {
-            if (x > 0) {
-                sum += x;
-                int r = x;
-                if (r > sum / 2) r = sum - r; 
-                long long current_nCr = 1;
-                for (int i = 1; i <= r; ++i) {
-                    current_nCr = (long long)((__int128)current_nCr * (sum - i + 1) / i);
-                    if (current_nCr > limit) {
-                        current_nCr = limit + 1;
-                        break;
+        for (int i = 0; i < n; i++) {
+            ct[s[i] - 'a']++;
+        }
+
+        int idx = 0;
+
+        long long num = 1;
+
+        int j = 25;
+        int running_mult = 1;
+        for (int i = 1; i <= n; i++) {
+            while (running_mult > ct[j]) {
+                running_mult = 1;
+                j--;
+            }
+
+            num *= i;
+            num /= running_mult;
+            running_mult++;
+
+            if (num > k) {
+                for (int m = 0; m < 26; m++) {
+                    while (idx + i < n && ct[m] > 0) {
+                        s[s.size() - idx - 1] = 'a' + m;
+                        s[idx++] = 'a' + m;
+                        ct[m]--;
                     }
                 }
-                
-                if (current_nCr > limit) return limit + 1;
-                
-                if (limit / ways < current_nCr) {
-                    return limit + 1;
+
+                break;
+            }
+        }
+
+        if (k > num) {
+            return "";
+        }
+
+        if (n == 0) {
+            return s;
+        }
+
+        while (idx < n && k > 0) {
+            for (int i = 0; i < 26; i++) {
+                if (ct[i] == 0) {
+                    continue;
+                }
+
+                long long span = num * ct[i] / (n - idx);
+
+                if (k == span) {
+                    k -= span;
+                    s[s.size() - idx - 1] = 'a' + i;
+                    s[idx++] = 'a' + i;
+                    num = span;
+                    ct[i]--;
+                    break;
+                } else if (k > span) {
+                    k -= span;
                 } else {
-                    ways *= current_nCr;
+                    s[s.size() - idx - 1] = 'a' + i;
+                    s[idx++] = 'a' + i;
+                    num = span;
+                    ct[i]--;
+                    break;
                 }
             }
         }
-        return ways;
+
+        for (int i = 25; i >= 0; i--) {
+            while (ct[i] > 0) {
+                s[s.size() - idx - 1] = 'a' + i;
+                s[idx++] = 'a' + i;
+                ct[i]--;
+            }
+        }
+
+        return s;
     }
 };
